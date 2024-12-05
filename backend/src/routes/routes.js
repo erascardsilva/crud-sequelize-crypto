05f849ab2cf6925ce03c6722aa555d43e254088d
erascardsilva <erasmo@vmi-informatica.com.br>
2024-12-05 02:21:36 -0300

    finalizado jwt

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,10 +6,10 @@ const { auth } = require('../middleware/auth');
 router.post('/login', userController.login);
 
 router.post('/create', userController.createUser);
-router.get('/buscar', userController.getAllUsers);
-router.get('/buscar/:id', userController.getUserById);
-router.put('/atualizar/:id', userController.updateUser);
-router.delete('/deleta/:id', userController.deleteUser);
+router.get('/buscar', auth , userController.getAllUsers);
+router.get('/buscar/:id', auth , userController.getUserById);
+router.put('/atualizar/:id', auth, userController.updateUser);
+router.delete('/deleta/:id', auth ,userController.deleteUser);
 
 
 module.exports = router;
